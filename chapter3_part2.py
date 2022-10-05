6c637589fa1142c058623fe17155c515e62c1a18
labeleeanny <https://github.com/labeleeanny>
2022-10-05 16:10:38 +0300

    add tasks for chapter 3

---
+++ chapter3_part2.py
@@ -0,0 +1,23 @@
+# task 1
+print("*****************", end="\n")
+print("*               *", end="\n")
+print("*               *", end="\n")
+print("*****************")
+
+
+# task 2
+a, b = int(input()), int(input())
+print("Квадрат суммы", a, "и", b, "равен", (a + b) ** 2)
+print("Сумма квадратов", a, "и", b, "равна", a ** 2 + b ** 2)
+
+
+# task 3
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+print(a ** b + c ** d)
+
+
+# step 4
+n = int(input())
+nn = n * 11
+nnn = n * 111
+print(n + nn + nnn)
